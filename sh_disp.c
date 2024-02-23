2a22b81dcbea0e33aca2d9faf9d8cfa12865d56e
cybermariam <ademolamariam95@gmail.com>
2024-02-22 21:39:49 -0800

    added execve,input,printf,disp files

---
+++ sh_disp.c
@@ -0,0 +1,8 @@
+#include "sshell.h"
+/**
+ * prompt_disp - A function that prompt user input message
+ */
+void prompt_disp(void)
+{
+	sh_printf("($) ");
+}
---
+++ sh_exe.c
@@ -0,0 +1,41 @@
+#include "sshell.h"
+
+/**
+ * sh_execmd - A function that execute commands
+ * @cmd: Command
+ * @sh_name: The name of the shell xcutable file
+ */
+void sh_execmd(char **cmd, char *sh_name)
+{
+	pid_t pid;
+	char *token;
+	char *cmd_tokens[128];
+	char *env_paths[1] = {NULL};
+	int count = 0;
+
+	/*Creating a child process*/
+	pid = fork();
+	if (pid == 0)
+	{
+		token = strtok(*cmd, " \t\n");
+		while (token != NULL)
+		{
+			cmd_tokens[count++] = token;
+			token = strtok(NULL, " \t\n");
+		}
+		/*Ensure the last item is NULL*/
+		cmd_tokens[count] = NULL;
+		execve(*cmd, cmd_tokens, env_paths);
+		perror(sh_name);
+		exit(EXIT_FAILURE);
+	}
+	else if (pid > 0)
+	{
+		wait(NULL);
+	}
+	else
+	{
+		perror("Fork Failed");
+		exit(EXIT_FAILURE);
+	}
+}
